f14a09999d4ea2593fa045e18b3d642d57bdd56b
avichaig <32450776+avichaig@users.noreply.github.com>
2018-01-23 15:40:37 +0200

    mod03 demo coded
    new demo code added

---
+++ Allfiles/Mod03/Democode/HtmlDOMSample/HtmlDOMSample/Scripts/IndexScript.js
@@ -0,0 +1,30 @@
+﻿function addHobbies() {
+    //Get all hobbies inputs
+    const inputList = document.querySelectorAll('.hobbiesInput');
+    //check if hobbies input elements less then 5
+    if (inputList.length < 5) {
+        //Get hobbies input list div
+        const hobbiesList = document.getElementById('hobbiesList');
+        //Create new elements
+        const newLineElement = createNode('br'),
+            createNode('input');
+        //Set class attribute to the new input element
+        inputElement.setAttribute("class", "hobbiesInput");
+        //Insert the new elements to the end of the div
+        append(hobbiesList, newLineElement);
+        append(hobbiesList, inputElement);
+    }
+    else {
+        alert("Can't add more hobbies (max: 5)");
+    }
+}
+
+//Create new element by element name
+function createNode(element) {
+    return document.createElement(element);
+}
+
+// Append the element(el) into the parent
+function append(parent, el) {
+    return parent.appendChild(el);
+}
