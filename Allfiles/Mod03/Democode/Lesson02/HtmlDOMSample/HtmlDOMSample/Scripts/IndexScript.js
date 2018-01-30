2a92c8268435431b8c50d68058a03b1a0da9fb29
avichaig <32450776+avichaig@users.noreply.github.com>
2018-01-30 15:33:21 +0200

    replace index file

--- Allfiles/Mod03/Democode/Lesson02/HtmlDOMSample/HtmlDOMSample/Scripts/IndexScript.js
+++ Allfiles/Mod03/Democode/Lesson02/HtmlDOMSample/HtmlDOMSample/Scripts/IndexScript.js
@@ -1,4 +1,10 @@
-﻿function addHobbies() {
+﻿function init() {
+    document.getElementById('newHobbyBtn').addEventListener('click', addHobbies);
+}
+
+init();
+
+function addHobbies() {
     //Get all hobbies inputs
     const inputList = document.querySelectorAll('.hobbiesInput');
     //check if hobbies input elements less then 5
@@ -7,7 +13,7 @@
         const hobbiesList = document.getElementById('hobbiesList');
         //Create new elements
         const newLineElement = createNode('br'),
-            createNode('input');
+            inputElement = createNode('input');
         //Set class attribute to the new input element
         inputElement.setAttribute("class", "hobbiesInput");
         //Insert the new elements to the end of the div
@@ -15,7 +21,7 @@
         append(hobbiesList, inputElement);
     }
     else {
-        alert("Can't add more hobbies (max: 5)");
+        document.getElementById('newHobbyBtn').removeEventListener('click', addHobbies);
     }
 }
 
