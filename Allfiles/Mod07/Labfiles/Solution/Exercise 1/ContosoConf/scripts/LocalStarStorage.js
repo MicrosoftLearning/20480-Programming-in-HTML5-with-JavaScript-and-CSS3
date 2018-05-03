09d4350b2caa0cb525c7c2f17c9740a330de2de9
Unknown <avichaigez@gmail.com>
2018-05-03 15:19:22 +0300

    mod07_module_classes
    inherit and jquery removed
    modules and classes added

--- Allfiles/Mod07/Labfiles/Solution/Exercise 1/ContosoConf/scripts/LocalStarStorage.js
+++ Allfiles/Mod07/Labfiles/Solution/Exercise 1/ContosoConf/scripts/LocalStarStorage.js
@@ -1,59 +1,52 @@
-﻿/// <reference path="_namespace.js" />
-/// <reference path="Object.inherit.js" />
+﻿export class LocalStarStorage {
+    constructor(localStorage) {
+        /// <summary>Encapsulates the browser's localStorage API to make saving a list of starred sessions easy.</summary>
+        this.localStorage = localStorage;
+        this.load();
+    }
 
-(function () {
+    addStar(sessionId) {
+        /// <summary>Stars the given session.</summary>
+        if (this.isStarred(sessionId)) return;
 
-    conference.LocalStarStorage = Object.inherit({
+        this.sessions.push(sessionId);
+        this.save();
+    }
 
-        initialize: function (localStorage) {
-            /// <summary>Encapsulates the browser's localStorage API to make saving a list of starred sessions easy.</summary>
-            this.localStorage = localStorage;
-            this.load();
-        },
-
-        addStar: function (sessionId) {
-            /// <summary>Stars the given session.</summary>
-            if (this.isStarred(sessionId)) return;
-
-            this.sessions.push(sessionId);
+    removeStar(sessionId) {
+        /// <summary>Removes the star from the given session.</summary>
+        const index = this.sessions.indexOf(sessionId);
+        if (index >= 0) {
+            this.sessions.splice(index, 1);
             this.save();
-        },
+        }
+    }
 
-        removeStar: function (sessionId) {
-            /// <summary>Removes the star from the given session.</summary>
-            const index = this.sessions.indexOf(sessionId);
-            if (index >= 0) {
-                this.sessions.splice(index, 1);
-                this.save();
-            }
-        },
+    isStarred(sessionId) {
+        /// <summary>Returns true if the given session is starred.</summary>
+        return this.sessions.indexOf(sessionId) >= 0;
+    }
 
-        isStarred: function (sessionId) {
-            /// <summary>Returns true if the given session is starred.</summary>
-            return this.sessions.indexOf(sessionId) >= 0;
-        },
-
-        load: function () {
-            /// <summary>Loads the starred sessions from storage.</summary>
-            const json = this.localStorage.getItem("stars");
-            if (json) {
-                try {
-                    this.sessions = JSON.parse(json) || [];
-                } catch (exception) {
-                    this.sessions = [];
-                }
-            } else {
+    load() {
+        /// <summary>Loads the starred sessions from storage.</summary>
+        const json = this.localStorage.getItem("stars");
+        if (json) {
+            try {
+                this.sessions = JSON.parse(json) || [];
+            } catch (exception) {
                 this.sessions = [];
             }
-        },
-
-        save: function () {
-            /// <summary>Saves the starred sessions to storage.</summary>
-            this.localStorage.setItem("stars", JSON.stringify(this.sessions));
+        } else {
+            this.sessions = [];
         }
-    });
+    }
 
-} ());
+    save() {
+        /// <summary>Saves the starred sessions to storage.</summary>
+        this.localStorage.setItem("stars", JSON.stringify(this.sessions));
+    }
+}
+
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
 // SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
