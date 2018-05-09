0406c87c446c8600e78c928fb623176723ed9964
Unknown <avichaigez@gmail.com>
2018-05-09 18:33:18 +0300

    change modules and classes
    model 07 starter and solution changed and now using modules and classes

---
+++ Allfiles/Mod07/Labfiles/Solution/Exercise 2/ContosoConf/scripts/ScheduleList.js
@@ -0,0 +1,41 @@
+import { ScheduleItem } from "./ScheduleItem.js";
+
+export class ScheduleList {
+    constructor(element, localStarStorage) {
+        this.element = element;
+        this.localStarStorage = localStarStorage;
+    }
+
+    // TODO: Refactor these functions into methods of the ScheduleList object.
+
+    async startDownload() {
+        // await response of fetch call
+        let response = await fetch("/schedule/list")
+        // transform body to json
+        let data = await response.json();
+
+        // checking response is ok
+        if (response.ok) {
+            this.downloadDone(data);
+        } else {
+            this.downloadFailed();
+        }
+    }
+
+    downloadDone(responseData) {
+        this.addAll(responseData.schedule);
+    }
+
+    downloadFailed() {
+        alert("Could not retrieve schedule data at this time. Please try again later.");
+    }
+
+    addAll(itemsArray) {
+        itemsArray.forEach(this.add, this); // TODO: When refactoring this, add the `this` argument to `forEach`.
+    }
+
+    add(itemData) {
+        const item = new ScheduleItem(itemData, this.localStarStorage);
+        this.element.appendChild(item.element);
+    }
+}
